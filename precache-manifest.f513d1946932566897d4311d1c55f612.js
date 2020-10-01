6102f2a1923aed0793ca547cbd5d3996cdfc1d29
mar1an4ik <69079587+mar1an4ik@users.noreply.github.com>
2020-10-01 06:56:55 +0300

    Updates

---
+++ precache-manifest.f513d1946932566897d4311d1c55f612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5852d8f17b5578fbdfab6ba5a64812cd",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "2076849e38857306f23f",
+    "url": "/movies/static/css/main.4db86ef4.chunk.css"
+  },
+  {
+    "revision": "2fa457e1631e353773b5",
+    "url": "/movies/static/js/2.b8aa2a17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies/static/js/2.b8aa2a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2076849e38857306f23f",
+    "url": "/movies/static/js/main.25e2198b.chunk.js"
+  },
+  {
+    "revision": "3bb3c64f73988286cfcd",
+    "url": "/movies/static/js/runtime-main.df134f82.js"
+  }
+]);
